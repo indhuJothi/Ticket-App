cb8adacfae713e240c2a4a013afb5a280195c4e2
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-07 23:02:30 +0530

    Third Commit

--- bus-ticket-app/src/Reg-component/Regitration.js
+++ bus-ticket-app/src/Reg-component/Regitration.js
@@ -1,24 +1,19 @@
 import React from "react";
 import classes from './style.module.css'
 
-
-
 class Registration extends React.Component{
     constructor(props){
         super(props);
         this.state={name:'',
-                    dob:'',email:'',mobile:'',roundway:'',oneway:'',nameErr:''};
+                    dob:'',email:'',mobile:'',roundway:'',oneway:'',nameErr:'',dobErr:'',emailErr:'',mobileErr:'',radioErr:'',fromErr:''};
         this.handlechange=this.handlechange.bind(this);
         this.handlesubmit=this.handlesubmit.bind(this);
         this.onValueChange=this.onValueChange.bind(this);
     }
     onValueChange(event) {
         this.setState({
-          selectedOption: event.target.value,
-
-    
-        });
-      
+          selectedOption: event.target.value
+         });
       }
    
     handlechange(event){
@@ -40,22 +35,100 @@ class Registration extends React.Component{
                   }
               )
     }
+  
     valid=()=>{
-        let nameErr=''
-        if(this.state.name=='')
-        {
-            nameErr='Enter your name'
-        }
-        if(nameErr){
-            this.setState({nameErr});
-            return false
-
-        }
-        
+        let nameres=true,dobres=true,emailres=true,mobileres=true,radiores=true,fromres=true;
+        let nameErr='',dobErr='',emailErr='',mobileErr='',radioErr='',fromErr='';
+      
+        var nameregex=/^[a-zA-Z\s]{3,15}$/,
+        dobregex=/^\d{4}\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])$/,
+        emailregex=/^[A-Za-z0-9+_.-]+@(.+)+\.(.+)$/,
+        mobileregex = /^[6-9]\d{9}$/;
+            if(this.state.name==='')
+              {
+                  nameErr='Enter your name';
+                 this.setState({nameErr});
+                 nameres= false;
+            }
+          else{
+         
+              if(nameregex.test(this.state.name)===false)
+              {
+                  nameErr="Enter a valid name";
+                  this.setState({nameErr});
+                  nameres= false;
+              }
+              else{
+                this.setState({nameErr});
+                nameres= true;
+                }
+            }
+          if(this.state.dob==='')
+            {
+                dobErr='Enter your DOB';
+                dobres= false;
+                this.setState({dobErr});
+               
+            }
+            else{
+                if(dobregex.test(this.state.dob)===false){
+                    dobErr='Enter a valid DOB';
+                    this.setState({dobErr});
+                    dobres=false;
+                }
+                else{
+                    this.setState({dobErr});
+                    dobres=true;
+                }
+            }
+            if(this.state.email===''){
+                emailErr='Enter your Email ';
+                this.setState({emailErr});
+                emailres=false;
+            }
+            else{
+                if(emailregex.test(this.state.email)===false){
+                    emailErr='Enter a valid email';
+                    this.setState({emailErr});
+                    emailres=false;
+                }
+                else{
+                    this.setState({emailErr})
+                    emailErr=true;
+                }
+            }
+            if(this.state.mobile===''){
+                mobileErr="Enter your mobile";
+                this.setState({mobileErr});
+                mobileres=false;
+            }
+            else{
+                if(mobileregex.test(this.state.mobile)===false){
+                    mobileErr="Enter a valid Number";
+                    this.setState({mobileErr});
+                    mobileres=false;
+                }
+                else{
+                    this.setState({mobileErr})
+                    mobileres=true;
+                }
+            }
+            if(!this.state.selectedOption){
+                radioErr='Choose any one option';
+                this.setState({radioErr});
+                radiores=false;
+            }
+            else{
+                this.setState({radioErr});
+                radiores=true;
+            }
     }
+  
     handlesubmit(event){
         event.preventDefault();
-        const isvalid=this.valid();
+        this.valid();
+     
+        
      }
   
     render(){
@@ -71,39 +144,40 @@ class Registration extends React.Component{
                 <div class='dob'>
                     <label class={classes.label}>DOB: </label>
                     <input class={classes.input} name='dob' placeholder='yyyy/mm/dd'type='text' value={this.state.dob} onChange={this.handlechange}/>
-               <div class={classes.error}>{this.dobErr}</div>
+               <div class={classes.error}>{this.state.dobErr}</div>
                 </div>
                 <div class='email'>
                     <label class={classes.label}>email: </label>
                     <input type='email' class={classes.input} name='email' placeholder="abc@domain.com"type='text' value={this.state.email} onChange={this.handlechange}/>
-               <div ></div>
+                    <div class={classes.error} >{this.state.emailErr}</div>
+           
                 </div>
                 <div class='mobile'>
                     <label class={classes.label}>Mobile: </label>
                     <input type='text' class={classes.input} name='mobile' placeholder='Mobile Number' type='text' value={this.state.mobile} onChange={this.handlechange}/>
-               <div ></div>
+                    <div class={classes.error}>{this.state.mobileErr}</div>
                 </div>
                 <div class={classes.travetype}>
                     <label class={classes.label}>Oneway: </label>
-                    <input class={classes.onelabel} value='oneway' type='radio' checked={this.state.selectedOption =="oneway"}
+                    <input class={classes.onelabel} value='oneway' type='radio' checked={this.state.selectedOption ==="oneway"}
               onChange={this.onValueChange}/>
                     <label class={classes.label} >Roundway: </label>
                     <input class={classes.onelabel} value='roundway' type='radio' checked={this.state.selectedOption === "roundway"}
               onChange={this.onValueChange}/>            
-               <div ></div>
+               <div class={classes.error} >{this.state.radioErr}</div>
                 </div>
                
           
              {this.state.selectedOption==='oneway'?  
                <div   >
                <label class={classes.label}>From</label>
-               <select class={classes.input} onChange={this.handlechange}>
-                  <option value={this.state.select}>select</option>
-                   <option value={this.state.Chennai}>Chennai</option>
-                   <option value={this.state.Cuddalore}>Cuddalore</option>
-                   <option value={this.state.Trichy}>Trichy</option>
+               <select class={classes.input} class='select' onChange={this.handlechange}>
+                  <option value='select' >select</option>
+                   <option value='chennai'>Chennai</option>
+                   <option value='Cuddalore' >Cuddalore</option>
+                   <option value='Trichy'>Trichy</option>
                </select>
-               <div class={classes.error}></div>
+               <div class={classes.error}>{this.state.fromErr}</div>
                <div >
                 <label class={classes.label}>To</label>
                 <select classe={classes.input}>
